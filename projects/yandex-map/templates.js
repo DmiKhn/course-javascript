d7080cf746b077c0c6c2dfb0b87f3a157e55ef75
Dmitry Khanenya <dmikhn.work@gmail.com>
2022-12-10 13:44:01 +0300

    yandex-map first push

---
+++ projects/yandex-map/templates.js
@@ -0,0 +1,19 @@
+export const formTemplate = `
+  <form class="input-form" id="add-form">
+    <h3>Отзыв:</h3>
+    <input class="input-place" type="text" placeholder="Укажите ваше имя" name="place"><br><br>
+    <input class="input-name" type="text" placeholder="Укажите место" name="author"><br><br>
+    <textarea class="review-text" placeholder="Оставить отзыв" name="review"></textarea><br><br>
+    <button class="btn" id="ad-btn">Добавить</button><br>
+  </form>
+`;
+
+export const reviewTemplate = (review) => {
+  return `
+    <div class="review">
+      <div><strong>Место: </strong>${review.place}</div>
+      <div><strong>Имя: </strong>${review.author}</div>
+      <div><strong>Отзыв: </strong>${review.reviewText}</div>
+    </div>
+  `;
+};
